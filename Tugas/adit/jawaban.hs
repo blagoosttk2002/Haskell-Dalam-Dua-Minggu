5f44c8365b2fc5e626ec64dbc70e50d26bf5d438
mra128 <aditrizky128@gmail.com>
2025-04-01 19:45:47 +0700

    membuat file jawaban.hs

---
+++ Tugas/adit/jawaban.hs
@@ -0,0 +1,268 @@
+-- First Assignment
+-- Reimplement Haskell function
+-- DON'T USE GOOGLE
+module Template where
+
+import Data.List
+
+-- 1.a
+
+-- Difficulty: Easy
+null' x = x
+
+--pembatas
+
+-- Difficulty: Medium
+take' x = x
+
+--pembatas
+
+-- Difficulty: Medium
+drop' x = x
+
+--pembatas
+
+-- Difficulty: Very Easy
+fst' x = x
+
+--pembatas
+
+-- Difficulty: Very Easy
+snd' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+map' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+filter' x = x
+
+--pembatas
+
+-- Difficulty: Medium
+delete' x = x
+
+--pembatas
+
+-- Difficulty: Medium
+deleteAll' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+foldl' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+foldl1' x = x
+
+--pembatas
+
+-- Difficulty: Medium
+zip' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+zipWith' x = x
+
+--pembatas
+
+-- Difficulty: Easy
+nth' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+scanl' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+scanl1' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+elem' x = x
+
+--pembatas
+
+-- Difficulty: Hard
+notElem' x = x
+
+--pembatas
+
+-- Difficulty: Very Easy
+head' x = x
+
+--pembatas
+
+-- Difficulty: Medium
+length' x = x
+
+--pembatas
+
+-- Difficulty: Medium
+reverse' x = x
+
+--pembatas
+
+-- Difficulty: Very Easy
+last' x = x
+
+--pembatas
+
+-- Difficulty: Very Easy
+tail' x = x
+
+--pembatas
+
+-- Difficulty: Very Easy
+init' x = x
+
+--pembatas
+
+
+-- Difficulty: Easy
+max' x = x
+
+--pembatas
+
+-- Difficulty: Easy
+min' x = x
+
+--pembatas
+
+concat' x = x
+
+--pembatas
+
+intersperse' x = x
+
+--pembatas
+
+intercalate' x = x
+
+--pembatas
+
+and' x = x
+
+--pembatas
+
+or' x = x
+
+--pembatas
+
+zip3' x = x
+
+--pembatas
+
+sum' x = x
+
+--pembatas
+
+product' x = x
+
+--pembatas
+
+words' x = x
+
+--pembatas
+
+lines' x = x
+
+--pembatas
+
+unlines' x = x
+
+--pembatas
+
+unwords' x = x
+
+--pembatas
+
+takeWhile' x = x
+
+--pembatas
+
+dropWhile' x = x
+
+--pembatas
+
+concatMap' x = x
+
+--pembatas
+
+all' x = x
+
+--pembatas
+
+any' x = x
+
+--pembatas
+
+insert' x = x
+
+--pembatas
+
+zipWith3' x = x
+
+--pembatas
+
+-- 1.b
+
+nub' x = x
+
+--pembatas
+
+sort' x = x
+
+--pembatas
+
+minimum' x = x
+
+--pembatas
+
+maximum' x = x
+
+--pembatas
+
+inits' x = x
+
+--pembatas
+
+tails' x = x
+
+--pembatas
+
+union' x = x
+
+--pembatas
+
+intersect' x = x
+
+--pembatas
+
+group' x = x
+
+--pembatas
+
+splitAt' x = x
+
+--pembatas
+
+partition' x = x
+
+--pembatas
+
+replicate' x = x
+
+--pembatas
+-- First Assignment
+-- Reimplement Haskell function
+-- DON'T USE GOOGLE
